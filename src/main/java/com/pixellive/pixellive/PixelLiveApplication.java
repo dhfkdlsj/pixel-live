0c7090db162f3aed87c23443ee11e932836ca6d7
dhfkdlsj <dhfkdlsj1@gmail.com>
2025-12-11 21:34:36 +0900

    feat: init project structure

---
+++ src/main/java/com/pixellive/pixellive/PixelLiveApplication.java
@@ -0,0 +1,13 @@
+package com.pixellive.pixellive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelLiveApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PixelLiveApplication.class, args);
+  }
+
+}
